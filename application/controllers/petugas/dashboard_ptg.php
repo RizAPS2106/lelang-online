9a9789ffa9058751ecb89a518889d0d85162644c
rizky <rizkyaditya2106@gmail.com>
2022-05-20 16:29:15 +0700

    Adjusting

--- application/controllers/petugas/dashboard_ptg.php
+++ application/controllers/petugas/dashboard_ptg.php
@@ -1,8 +1,8 @@
-<?php  
+<?php
 
 class Dashboard_ptg extends CI_Controller
 {
-	
+
 	public function index()
 	{
 		$data['jumlah_masyarakat'] = $this->m_dashboard->jumlah_data('masyarakat');
@@ -14,7 +14,7 @@ class Dashboard_ptg extends CI_Controller
 
 		$this->load->view('templates_ptg/header');
 		$this->load->view('templates_ptg/navbar');
-		$this->load->view('petugas/dashboard_ptg',$data);
+		$this->load->view('petugas/dashboard_ptg', $data);
 		$this->load->view('templates_ptg/footer');
 	}
 
@@ -24,7 +24,7 @@ class Dashboard_ptg extends CI_Controller
 
 		$data['masyarakat'] = $this->m_dashboard->tampil_data('masyarakat');
 
-		$this->load->view('petugas/laporan/laporan_masyarakat',$data);
+		$this->load->view('petugas/laporan/laporan_masyarakat', $data);
 
 		$paper_size = 'A4';
 		$orientation = 'landscape';
@@ -33,7 +33,7 @@ class Dashboard_ptg extends CI_Controller
 		$this->dompdf->set_paper($paper_size, $orientation);
 		$this->dompdf->load_html($html);
 		$this->dompdf->render();
-		$this->dompdf->stream('Laporan_Masyarakat.pdf',array('Attachment' => 0));
+		$this->dompdf->stream('Laporan_Masyarakat.pdf', array('Attachment' => 0));
 	}
 
 	public function report_ptg()
@@ -42,7 +42,7 @@ class Dashboard_ptg extends CI_Controller
 
 		$data['petugas'] = $this->m_dashboard->tampil_data('petugas');
 
-		$this->load->view('petugas/laporan/laporan_petugas',$data);
+		$this->load->view('petugas/laporan/laporan_petugas', $data);
 
 		$paper_size = 'A4';
 		$orientation = 'landscape';
@@ -51,23 +51,23 @@ class Dashboard_ptg extends CI_Controller
 		$this->dompdf->set_paper($paper_size, $orientation);
 		$this->dompdf->load_html($html);
 		$this->dompdf->render();
-		$this->dompdf->stream('Laporan_Petugas.pdf',array('Attachment' => 0));
+		$this->dompdf->stream('Laporan_Petugas.pdf', array('Attachment' => 0));
 	}
 
 	public function report_brg()
 	{
-		if ($this->input->post('from') AND $this->input->post('to')) {
+		if ($this->input->post('from') and $this->input->post('to')) {
 			$data['from'] = date('Y-m-d', strtotime($this->input->post('from')));
 			$data['to'] = date('Y-m-d', strtotime($this->input->post('to')));
 
-			$data['barang'] = $this->m_dashboard->tampil_data_from_to_brg($data['from'],$data['to']);
-		}else{
+			$data['barang'] = $this->m_dashboard->tampil_data_from_to_brg($data['from'], $data['to']);
+		} else {
 			$data['barang'] = $this->m_dashboard->tampil_data('barang');
 		}
 
 		$this->load->library('dompdf_gen');
 
-		$this->load->view('petugas/laporan/laporan_barang',$data);
+		$this->load->view('petugas/laporan/laporan_barang', $data);
 
 		$paper_size = 'A4';
 		$orientation = 'landscape';
@@ -76,23 +76,23 @@ class Dashboard_ptg extends CI_Controller
 		$this->dompdf->set_paper($paper_size, $orientation);
 		$this->dompdf->load_html($html);
 		$this->dompdf->render();
-		$this->dompdf->stream('Laporan_Barang.pdf',array('Attachment' => 0));
+		$this->dompdf->stream('Laporan_Barang.pdf', array('Attachment' => 0));
 	}
 
 	public function report_llg()
 	{
-		if ($this->input->post('from') AND $this->input->post('to')) {
+		if ($this->input->post('from') and $this->input->post('to')) {
 			$data['from'] = date('Y-m-d', strtotime($this->input->post('from')));
 			$data['to'] = date('Y-m-d', strtotime($this->input->post('to')));
 
-			$data['lelang'] = $this->m_dashboard->tampil_data_from_to_llg($data['from'],$data['to']);
-		}else{
+			$data['lelang'] = $this->m_dashboard->tampil_data_from_to_llg($data['from'], $data['to']);
+		} else {
 			$data['lelang'] = $this->m_dashboard->tampil_data_lelang();
 		}
 
 		$this->load->library('dompdf_gen');
 
-		$this->load->view('petugas/laporan/laporan_lelang',$data);
+		$this->load->view('petugas/laporan/laporan_lelang', $data);
 
 		$paper_size = 'A4';
 		$orientation = 'landscape';
@@ -101,7 +101,6 @@ class Dashboard_ptg extends CI_Controller
 		$this->dompdf->set_paper($paper_size, $orientation);
 		$this->dompdf->load_html($html);
 		$this->dompdf->render();
-		$this->dompdf->stream('Laporan_Lelang.pdf',array('Attachment' => 0));
+		$this->dompdf->stream('Laporan_Lelang.pdf', array('Attachment' => 0));
 	}
-	
 }
